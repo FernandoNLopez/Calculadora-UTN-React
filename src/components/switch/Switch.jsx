d4aeabd7a06fd41c9cb529e0c146ab3818b5b872
FernandoNLopez <lopezfer1998@outlook.com>
2023-11-24 16:45:50 -0300

    second

---
+++ src/components/switch/Switch.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+import './switch.css';
+
+const Switch = () => {
+
+
+  const [theme, setTheme] = useState('claro');
+
+  const handleChange = (e) => setTheme(e.target.checked ? 'oscuro' : 'claro');
+
+  useEffect(() => {
+    document.body.setAttribute('data-theme', theme)
+  }, [theme]);
+
+  return (
+    <section className='switch'>
+      <label className='toggle'>
+        <input type="checkbox" className='check-switch' hidden onChange={handleChange}/>
+        <span className='slider' />
+      </label>
+    </section>
+  )
+}
+
+export default Switch;
